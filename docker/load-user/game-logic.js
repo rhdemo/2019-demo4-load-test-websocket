f217fcbbb9f62b20545489aca31c1d3c446f302d
Guilherme Baufaker Rêgo <gbaufake@redhat.com>
2019-04-09 16:25:52 +0200

    Renaming Gitignore

--- docker/load-user/game-logic.js
+++ docker/load-user/game-logic.js
@@ -1,15 +1,15 @@
 module.exports = { gameMock };
-var nanoid = require('nanoid')
-var jsonfile = require('jsonfile')
+var nanoid = require('nanoid');
+var jsonfile = require('jsonfile');
 const uuidv1 = require('uuid/v1');
-const file = 'sample.json'
+const file = 'sample.json';
 var gesture;
 
-gesture = jsonfile.readFileSync(file)
+gesture = jsonfile.readFileSync(file);
 
 
 function gameMock(userContext, events, done) {
 
-    userContext.vars.data = Object.assign({}, gesture, { uuid: nanoid() })
+    userContext.vars.data = Object.assign({}, gesture, { uuid: nanoid() });
     done();
 }
