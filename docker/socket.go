4988c67b767ad485d20ba60fba5090d1607586bb
Guilherme Baufaker Rêgo <gbaufake@redhat.com>
2020-02-27 23:54:59 -0300

    Moving to 2020

--- docker/socket.go
+++ docker/socket.go
@@ -14,12 +14,13 @@ import (
 
 	"github.com/sacOO7/gowebsocket"
 
-	uuid "github.com/satori/go.uuid"
+	// uuid "github.com/satori/go.uuid"
 )
 
 type user struct {
 	playerID string
 	gameID   string
+	score  float64
 	socket   gowebsocket.Socket
 }
 
@@ -40,7 +41,7 @@ func main() {
 
 func handleSocket(clientNumber int, socketAddress string, movement string) {
 
-	socket := gowebsocket.New(os.Getenv("SOCKET_ADDRESS"))
+	socket := gowebsocket.New(socketAddress)
 
 	u := user{playerID: "", gameID: "", socket: socket}
 
@@ -56,19 +57,20 @@ func handleSocket(clientNumber int, socketAddress string, movement string) {
 	u.socket.OnConnected = func(socket gowebsocket.Socket) {
 		log.Println(strconv.Itoa(clientNumber) + " connected")
 		if u.socket.IsConnected == true {
-			u.socket.SendBinary([]byte(`{ "type": "connection"}`))
+			u.socket.SendBinary([]byte(`{ "type": "init", "bot": "true"}`))
 		}
 	}
 
 	u.socket.OnTextMessage = func(message string, socket gowebsocket.Socket) {
 		messageResult := convertJSON(message)
 
-		if messageResult["type"].(string) == "configuration" {
-			u.playerID = messageResult["playerId"].(string)
-			u.gameID = messageResult["gameId"].(string)
+		if messageResult["type"].(string) == "player-configuration" {
+			player := messageResult["player"].(map[string]interface{})
+			u.playerID = player["id"].(string)
+			u.gameID = player["gameId"].(string)
+			u.score = player["score"].(float64)
 			log.Println(strconv.Itoa(clientNumber) + " became " + u.playerID)
-		} else if messageResult["type"].(string) == "motion_feedback" {
-			log.Println(u.playerID + " received message - " + message)
+			log.Println( u.playerID + " score: " + fmt.Sprintf("%f", u.score)
 		}
 	}
 	u.socket.Connect()
@@ -97,7 +99,7 @@ func reconnect(u user) {
 		log.Println("User: " + u.playerID + " is reconnecting")
 		time.Sleep(2 * time.Second)
 
-		reconnectPayload := fmt.Sprintf(`{ "type": "connection", "gameId": "%s", "playerId": "%s" }`, u.playerID, u.gameID)
+		reconnectPayload := fmt.Sprintf(`{ "type": "init", "bot": "true", "gameId": "%s", "playerId": "%s" }`, u.playerID, u.gameID)
 		u.socket = gowebsocket.New(os.Getenv("SOCKET_ADDRESS"))
 		u.socket.Connect()
 		u.socket.SendBinary([]byte(reconnectPayload))
@@ -108,48 +110,34 @@ func reconnect(u user) {
 func selectRandomMovement(movement string) (string, string) {
 	var n int
 	moves := []string{
-		"floss.json",
-		"fever.json",
-		"roll.json",
-		"shake.json",
-		"x.json",
-		"circle.json",
-		"bad-move.json",
+		"bad.json",
+		"good.json",
 	}
 	switch movement {
-	case "FLOSS":
-		n = 0
-	case "FEVER":
-		n = 1
-	case "ROLL":
-		n = 2
-	case "SHAKE":
-		n = 3
-	case "X":
-		n = 4		
-	case "CIRCLE":
-		n = 5
 	case "BAD":
-		n = 6
-	case "NO-BAD-DANCE":
-		n = rand.Int() % (len(moves)-1)
+		n = 0
+	case "GOOD":
+		n = 1
+	// case "NO-BAD-DANCE":
+	// 	n = rand.Int() % (len(moves)-1)
 	case "RANDOM":
 		n = rand.Int() % len(moves)
 	}
 
-	moveSelected := "moves/" + moves[n]
+	moveSelected := "guesses/" + moves[n]
 	movelog := strings.TrimSuffix(moves[n], filepath.Ext(moves[n]))
 	return moveSelected, movelog
 }
 
 func createPayload(playerID string, movement string) []byte {
+
+
 	jsonFile, _ := os.Open(movement)
 	byteValue, _ := ioutil.ReadAll(jsonFile)
 
 	var data map[string]interface{}
 	json.Unmarshal([]byte(byteValue), &data)
 	data["playerId"] = playerID
-	data["uuid"] = uuid.Must(uuid.NewV4()).String()
 	out, _ := json.Marshal(data)
 	return out
 }
